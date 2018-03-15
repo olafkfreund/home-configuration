54bfe6dbacdb2bc93a1752e301e71ef2a42d8fd2
Florent Clarret <florent.clarret@gmail.com>
2018-03-15 19:23:25 +0100

    Add ssh configuration and shortcuts

--- ssh_config.sh
+++ ssh_config.sh
@@ -20,9 +20,28 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-#!/bin/sh
+#! /bin/sh
 
-source ./functions.sh
-source ./aliases.sh
-source ./variables.sh
-source ./ssh_config.sh
+function start_agent {
+    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+    chmod 600 "${SSH_ENV}"
+    source "${SSH_ENV}" > /dev/null
+    /usr/bin/ssh-add;
+}
+
+SSH_ENV="$HOME/.ssh/ssh_agent_data"
+
+# Source SSH settings, if needed
+if [ -f "${SSH_ENV}" ]; then
+    source "${SSH_ENV}" > /dev/null
+    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+        start_agent;
+    }
+else
+    start_agent;
+fi
+
+## Fetch hostname from sshconfig and create an alias to connect to them
+for name in $(grep -E ^Host ~/.ssh/config | grep -v '*' | grep -v '?' | sed 's/^Host \(.*\)/\1/'); do
+	eval alias "$name=\"ssh $name\""
+done
