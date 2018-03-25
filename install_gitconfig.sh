6f73411831cd33f25a1a6f659400f098ac9aeb61
Florent Clarret <florent.clarret@gmail.com>
2018-03-25 13:18:08 +0200

    Improve the deployment script

--- install_gitconfig.sh
+++ install_gitconfig.sh
@@ -2,6 +2,56 @@
 
 set -e
 
-cp gitconfig ~/.gitconfig
+SELF=$(basename "$0")
 
-echo "Installed the git configuration successfully!"
+usage() {
+  echo "$SELF: "
+  echo "\tDeploy the gitconfig file to the home directly"
+  echo
+  echo "Parameters:"
+  echo "\tp: Pull from origin before deploying"
+  echo "\tr: Replace the gitconfig file if it already exists"
+  echo "\th: Show this usage"
+  exit 0
+}
+
+pull() {
+  echo "Pulling changes..."
+  git pull
+  echo "Pull done."
+  echo
+}
+
+deploy() {
+  echo "Deploying configuration file to home directory.."
+  cp gitconfig ~/.gitconfig
+  echo "Installed the git configuration successfully! Have fun"
+}
+
+
+# ================================== Main ==================================
+
+# Init variables
+PULL="NO"
+REPLACE="NO"
+
+while getopts ":pr" opt; do
+  case $opt in
+    p) PULL="YES";;
+    r) REPLACE="YES";;
+    \?) usage;;
+  esac
+done
+
+if [ "$PULL" == "YES" ]; then
+  pull
+fi
+
+if [[ -f ~/.gitconfig && "$REPLACE" == "NO" ]]; then
+  echo "ERROR : The configuration file already exists. Use -r to replace it."
+else
+  if [ -f ~/.gitconfig ]; then
+    echo "WARN : A gitconfig file already exists. It will be replaced."
+  fi
+  deploy
+fi
