6011ae100868513d73b95ff0361023211327a71f
Florent Clarret <florent.clarret@gmail.com>
2018-03-25 10:56:43 +0200

    Update PS1 for git

--- git/git.sh
+++ git/git.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 get_git_current_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' | sed -e 's/* \(.*\)/ (\1) /'
+  git branch 2> /dev/null | sed -e '/^[^*]/d' | sed -e 's/* \(.*\)/ (branch: \1) /'
 }
