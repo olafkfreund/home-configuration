96ba4b0ff4c4b3a72ed7c8053bb4fd2e674983db
Florent Clarret <florent.clarret@gmail.com>
2018-04-13 12:56:45 +0200

    Refactor deploy script

---
+++ install_home_configuration.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -e
+
+SELF=$(basename "$0")
+
+usage() {
+  echo "$SELF: "
+  echo "\tDeploy the alias files to the home directly"
+  exit 0
+}
+
+# ================================== Main ==================================
+
+echo "Deploy aliases..."
+rm -rf ~/.aliases
+mkdir -p ~/.aliases
+cp -R aliases-configuration ~/.aliases/
+echo "Aliases deployed."
+
+echo "Deploy gitconfig..."
+mkdir -p ~/.config/git/
+cp git-configuration/gitignore ~/.config/git/gitignore
+cp git-configuration/gitconfig ~/.gitconfig
+echo "Gitconfig deployed."
+
+echo "Deploy mc config..."
+mkdir -p ~/.config/mc
+cp mc-configuration/ini ~/.config/mc/ini
+echo "Mc config deployed."
+
+echo "Deploy vimrc..."
+mkdir -p ~/.vim_runtime
+cp -R vim-configuration/* ~/.vim_runtime/
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+echo "Vim configuration deployed."
+
+echo "Have fun !"
