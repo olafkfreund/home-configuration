1313843c88a078e6af3f69867241f3aadfcdb1cc
Florent Clarret <florent.clarret@gmail.com>
2018-08-16 20:41:02 +0200

    feat(alias): add docker aliases

--- aliases-configuration/aliases.sh
+++ aliases-configuration/aliases.sh
@@ -72,6 +72,21 @@ fi
 # tar 
 alias untar='tar -xvf'
 
+# docker
+alias dk='docker'
+# Get latest container ID
+alias dkl="docker ps -l -q"
+# Get container process
+alias dkps="docker ps"
+# Get process included stop container
+alias dkpsa="docker ps -a"
+# Get images
+alias dki="docker images"
+# Execute interactive container, e.g., $dex base /bin/bash
+alias dkex="docker exec -i -t"
+# Stop and Remove all containers
+alias dkrmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
+
 # miscellaneous
 alias ip="curl ifconfig.me"
 alias randpass="openssl rand -base64 24"
