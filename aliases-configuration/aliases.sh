1e31075bba0138c01d585b4b013976451139c9d4
Florent Clarret <florent.clarret@gmail.com>
2018-08-16 20:30:42 +0200

    feat(alias): autocomplete hosts for ssh connections

--- aliases-configuration/aliases.sh
+++ aliases-configuration/aliases.sh
@@ -64,5 +64,10 @@ if [ -f ~/.ssh/config ]; then
   done
 fi
 
+## SSH auto-completion based on entries in known_hosts.
+if [[ -e ~/.ssh/known_hosts ]]; then
+  complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
+fi
+
 # miscellaneous
 alias ip="curl ifconfig.me"
