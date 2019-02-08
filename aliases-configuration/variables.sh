10c7db95fc42a7b14d3a17c6b21a65fe5aff7f31
Florent Clarret <florent.clarret@gmail.com>
2019-02-08 22:14:21 +0100

    feat(shell): update history size to 8192

--- aliases-configuration/variables.sh
+++ aliases-configuration/variables.sh
@@ -29,6 +29,8 @@ export PAGER=most
 # History
 export HISTTIMEFORMAT="%d/%m/%y %T "
 export HISTCONTROL=ignoreboth:erasedups
+export HISTSIZE=8192
+export HISTFILESIZE=8192
 # Append to the history file instead of overwrite
 shopt -s histappend
 
