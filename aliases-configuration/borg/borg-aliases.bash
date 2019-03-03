c955e9e10f0b2c9b4f61e88b78281505ad17e57f
Florent Clarret <florent.clarret@gmail.com>
2019-03-03 16:35:20 +0100

    feat(borg): correctly exclude the .sync folder

--- aliases-configuration/borg/borg-aliases.bash
+++ aliases-configuration/borg/borg-aliases.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Some aliases for my personal manual backup that I use occasionally
-alias borg-home="borg create --verbose --list --stats --compression lzma,9 --exclude-caches --exclude .sync /mnt/backup/florentclarret/latitude/home.borg::\"Home-Latitude-{now}\" /home/florentclarret;";
-alias borg-home-no-music="borg create --verbose --list --stats --compression lzma,9 --exclude-caches --exclude .sync --exclude /home/florentclarret/Music /mnt/backup/florentclarret/latitude/home.borg::\"Home-Latitude-{now}\" /home/florentclarret;";
+alias borg-home="borg create --verbose --list --stats --compression lzma,9 --exclude-caches --exclude /home/florentclarret/.sync /mnt/backup/florentclarret/latitude/home.borg::\"Home-Latitude-{now}\" /home/florentclarret;";
+alias borg-home-no-music="borg create --verbose --list --stats --compression lzma,9 --exclude-caches --exclude /home/florentclarret/.sync --exclude /home/florentclarret/Music /mnt/backup/florentclarret/latitude/home.borg::\"Home-Latitude-{now}\" /home/florentclarret;";
