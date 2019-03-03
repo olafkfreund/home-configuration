3847939ebe1d26c631a020eb838901b0b61cf330
Florent Clarret <florent.clarret@gmail.com>
2019-03-03 16:15:38 +0100

    feat(borg): add some borg aliases to backup some folders

---
+++ aliases-configuration/borg/borg-aliases.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Some aliases for my personal manual backup that I use occasionally
+alias borg-home="borg create --verbose --list --stats --compression lzma,9 --exclude-caches --exclude .sync /mnt/backup/florentclarret/latitude/home.borg::\"Home-Latitude-{now}\" /home/florentclarret;";
+alias borg-home-no-music="borg create --verbose --list --stats --compression lzma,9 --exclude-caches --exclude .sync --exclude /home/florentclarret/Music /mnt/backup/florentclarret/latitude/home.borg::\"Home-Latitude-{now}\" /home/florentclarret;";
--- aliases-configuration/load.sh
+++ aliases-configuration/load.sh
@@ -4,6 +4,7 @@ ALIAS_FOLDER="$HOME/.aliases/"
 
 source "$ALIAS_FOLDER/git/git.sh"
 source "$ALIAS_FOLDER/screen/screen.sh"
+source "$ALIAS_FOLDER/borg/borg-aliases.bash"
 
 source "$ALIAS_FOLDER/ssh/start-agent.sh"
 
