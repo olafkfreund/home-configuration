f19a3784a57b4931dd60526858fe743f055ecc82
Florent Clarret <florent.clarret@gmail.com>
2019-03-04 21:53:43 +0100

    chore(completion): move ssh completion to a specific file

--- aliases-configuration/aliases.sh
+++ aliases-configuration/aliases.sh
@@ -63,11 +63,6 @@ if [[ -f ~/.ssh/config ]]; then
   done
 fi
 
-## SSH auto-completion based on entries in known_hosts.
-if [[ -e ~/.ssh/known_hosts ]]; then
-  complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
-fi
-
 # tar
 alias untar='tar -xvf'
 
