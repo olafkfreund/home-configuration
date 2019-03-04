ab14090f1e73c2ae2fc376179aaec075bc2d9a78
Florent Clarret <florent.clarret@gmail.com>
2019-03-04 21:55:13 +0100

    feat(completion): read the config file for ssh

--- completion/ssh.completion.bash
+++ completion/ssh.completion.bash
@@ -4,3 +4,8 @@
 if [[ -e ~/.ssh/known_hosts ]]; then
   complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
 fi
+
+## SSH auto-completion based on entries in config.
+if [[ -e ~/.ssh/known_hosts ]]; then
+  complete -W "$(grep -E ^Host ~/.ssh/config | grep -v '*' | grep -v '?' | sed 's/^Host \(.*\)/\1/' | tr ' ' '\n')" ssh
+fi
