389bdad6b68de000a34699909478922cdbcf05ba
Florent Clarret <florent.clarret@gmail.com>
2020-05-18 07:21:17 +0200

    feat(tmux): do not start tmux automatically

--- aliases-configuration/load.sh
+++ aliases-configuration/load.sh
@@ -17,6 +17,6 @@ done
 
 # Start tmux default session
 
-if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
-    tmux attach -t default || tmux new -s default
-fi
+#if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
+#    tmux attach -t default || tmux new -s default
+#fi
